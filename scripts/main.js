d915216ed1388163596f95338123da32457dc8bc
Egbealika511 <egbealika@gmail.com>
2022-08-22 12:35:53 +0100

    fixed the js code to display all conditions

--- scripts/main.js
+++ scripts/main.js
@@ -1,37 +1,65 @@
 let container = document.querySelector('.container');
-let startCountBtn = document.querySelector('.start-counter-btn');
-let stopCountBtn = document.querySelector('.stop-counter-btn');
 let displayCounterInput = document.querySelector('.display-counter');
 
+
 let counter = 60;
-let timeout;
-let timer_on = 0;
+setInterval (function (){
+    if (counter > 0){
+        displayCounterInput.value = counter;
+        counter -= 5;
+        switch (true) {
+            case counter % 2 === 0 && !counter % 5 === 0:
+                container.style.backgroundColor = "yellow";
+                container.style.color = "black";
+                break;
+            case counter % 5 === 0 && counter > 10:
+                container.style.backgroundColor = "green";
+                break;
+            case counter < 10:
+                container.style.backgroundColor = "red";
+        }
+    
+        console.log(counter);
+    } else {
+        container.style.backgroundColor = "hsl(0, 100%, 74%)";
+    }
+}, 1000)
 
-function countFunction () {
-    displayCounterInput.value = counter;
-    counter -= 5;
-    timeout = setTimeout(countFunction, 5000);
-}
 
-function startCount () {
-    if (!timer_on) {
-        timer_on = counter
-        countFunction()
-    }
-    if(counter % 2 === 0 || counter % 5 === 0) {
-        container.style.backgroundColor = 'yellow';
-    }
-    if (counter % 5 === 0){
-        container.style.backgroundColor = 'green';
-    }
-    if(counter < 10){
-        container.style.backgroundColor = 'red';
-    }
-    // container.style.backgroundColor = 'hsl(0, 100%, 74%)';
-    console.log(counter);
-}
 
-function stopCount () {
-    clearTimeout(timeout);
-    timer_on = 0;
-}
+
+
+
+
+// let counter = 60;
+// let timeout;
+// let timer_on = 0;
+
+// function countFunction () {
+//     displayCounterInput.value = counter;
+//     counter -= 5;
+//     timeout = setInterval(countFunction, 5000);
+// }
+
+// function startCount () {
+//     // if (!timer_on) {
+//     //     timer_on = counter
+//     //     countFunction()
+//     // }
+//     if(counter % 2 === 0 && !counter % 5 === 0) {
+//         container.style.backgroundColor = 'yellow';
+//     }
+//     if (counter % 5 === 0){
+//         container.style.backgroundColor = 'green';
+//     }
+//     if(counter < 10 && !counter % 5 === 0){
+//         container.style.backgroundColor = 'red';
+//     }
+//     // container.style.backgroundColor = 'hsl(0, 100%, 74%)';
+//     console.log(counter);
+// }
+
+// function stopCount () {
+//     clearTimeout(timeout);
+//     timer_on = 0;
+// }
